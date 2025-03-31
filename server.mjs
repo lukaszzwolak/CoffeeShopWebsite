f8592c39a1893ea90dfeb8fc1d0ffc6b5623b9f6
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-03-31 20:45:41 +0200

    add script: Express

--- server.mjs
+++ server.mjs
@@ -1,20 +1,24 @@
+import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import jsonServer from 'json-server';
 
-const server = jsonServer.create();
-const router = jsonServer.router(path.join('dist', 'db', 'app.json'));
-const middlewares = jsonServer.defaults({
-    static: 'dist',
-    noCors: true
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+const apiRouter = jsonServer.router(path.join(__dirname, 'dist/db/app.json'));
+const middlewares = jsonServer.defaults();
+app.use('/api', middlewares, apiRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-const port = process.env.PORT || 3131;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
-
-export default server;
-//npm install json-server
-//node server.mjs
+app.listen(PORT, () => {
+    console.log(`✅ Serwer działa na porcie ${PORT}`);
+});
