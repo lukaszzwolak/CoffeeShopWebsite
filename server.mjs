c8075ff47fe003e8cd5464d9e1559f7661d4fefd
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-04-01 10:04:43 +0200

    changes in server.mjs

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use('/api', jsonServer.defaults(), jsonServer.router(path.join(__dirname, 'src/db/app.json')));
 
