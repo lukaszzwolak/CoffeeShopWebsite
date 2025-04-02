a10a4003152e3e8b3c9ca51e42b43a26e9a98b65
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-04-02 09:12:06 +0200

    m

--- src/api/settings.js
+++ src/api/settings.js
@@ -1,5 +1,2 @@
-// export const API_URL = 'http://localhost:3131';
+export const API_URL = 'http://localhost:3131';
 
-const API_URL = window.location.origin.includes('replit.app')
-    ? window.location.origin + '/api'
-    : 'http://localhost:3131/api';
